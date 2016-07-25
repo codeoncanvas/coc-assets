3da4285d30f4b77ccea25d20c465a1912fe94a2b
lukasz karluk <julapy@googlemail.com>
2016-07-26 09:51:30 +1000

    empty cinder sample

---
+++ ci-samples/AssetsSample/src/AssetsSampleApp.cpp
@@ -0,0 +1,33 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "cocAssetsCI.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class AssetsSampleApp : public App {
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
+};
+
+void AssetsSampleApp::setup() {
+
+    //
+}
+
+void AssetsSampleApp::update() {
+    
+    //
+}
+
+void AssetsSampleApp::draw() {
+
+    //
+}
+
+CINDER_APP( AssetsSampleApp, RendererGl )
